13dc51baa2ab8a7e11e4681edd1ddd936dbe360c
Sean Keever <33592180+swkeever@users.noreply.github.com>
2026-09-21 18:08:07 -0700

    refactor(transport): type response header handling (#180)

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
   ],
   collectCoverageFrom: ['src/**/*.{js,ts}', '!src/**/*.d.ts'],
   coverageThreshold: {
+    'src/response-headers.ts': { branches: 100, functions: 100, lines: 100, statements: 100 },
     'src/response-json.ts': { branches: 100, functions: 100, lines: 100, statements: 100 },
     'src/fetch-lifecycle.ts': { branches: 100, functions: 100, lines: 100, statements: 100 },
     'src/durable-duration.ts': { branches: 100, functions: 100, lines: 100, statements: 100 },
