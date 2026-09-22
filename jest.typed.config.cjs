f2d7b3e6da7b011fe202b5714fb7ed79bdf78c6c
Sean Keever <33592180+swkeever@users.noreply.github.com>
2026-09-22 11:41:10 -0700

    test(sdk): discover all typed runtime coverage (#191)

---
+++ jest.typed.config.cjs
@@ -0,0 +1,12 @@
+const base = require('./jest.config.js');
+
+module.exports = {
+  ...base,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/generated/**'],
+  coverageThreshold: {
+    global: { branches: 100, functions: 100, lines: 100, statements: 100 },
+  },
+  coverageReporters: ['text', 'json', 'lcov'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+};
